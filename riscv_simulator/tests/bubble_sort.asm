74a839a8a7182e69ca40b2f40ba941257e2bc33d
Sudhanva Bharadwaj BM <138512597+bmsvinci1729@users.noreply.github.com>
2025-02-20 00:44:46 +0530

    Update bubble_sort.asm

--- riscv_simulator/tests/bubble_sort.asm
+++ riscv_simulator/tests/bubble_sort.asm
@@ -1,3 +1,13 @@
-addi x2 x0 2
-addi x3 x0 3
-add x4 x2 x3
+addi x1 x0 5
+addi x2 x0 10
+jal x3 loop
+addi x4 x0 20
+jal x16 exit  
+
+loop:
+    addi x5 x0 7
+    add x6 x1 x2
+    jalr x0 x3 0
+
+exit:
+    addi x17 x0 0
