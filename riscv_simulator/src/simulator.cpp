0977b9074ebbe30eb8409293a42bc712ec82ea62
Sudhanva Bharadwaj BM <138512597+bmsvinci1729@users.noreply.github.com>
2025-02-20 00:44:31 +0530

    Update simulator.cpp

--- riscv_simulator/src/simulator.cpp
+++ riscv_simulator/src/simulator.cpp
@@ -1,15 +1,24 @@
 #include "simulator.h"
+#include "cpu.h"
+#include "memory.h"
+#include "parser.h"
 #include <iostream>
 
 Simulator::Simulator() : cores{CPU(0), CPU(1), CPU(2), CPU(3)}, memory(4096) {}
 
-void Simulator::loadInstructions(std::vector<Instruction> instr) {
-    instructions = instr;
+void Simulator::loadInstructions(ParsedProgram parsedProgram) {
+    instructions = parsedProgram.instructions;
+    label_map = parsedProgram.label_map;  // Store label mapping
 }
 
 void Simulator::execute() {
-    for (int i = 0; i < instructions.size(); i++) {
-        cores[0].executeInstruction(instructions[i].opcode, instructions[i].operands);
+    int totalInstructions = instructions.size();
+    while (cores[0].pc / 4 < totalInstructions) {
+        int index = cores[0].pc / 4;
+        cores[0].executeInstruction(instructions[index].opcode, instructions[index].operands, label_map);
+        cores[1].executeInstruction(instructions[index].opcode, instructions[index].operands, label_map);
+        cores[2].executeInstruction(instructions[index].opcode, instructions[index].operands, label_map);
+        cores[3].executeInstruction(instructions[index].opcode, instructions[index].operands, label_map);
     }
 }
 
