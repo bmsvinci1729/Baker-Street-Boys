af19efdbbced7b6f3fda7d61f4959ba7d59a36b4
Sudhanva Bharadwaj BM <138512597+bmsvinci1729@users.noreply.github.com>
2025-02-20 00:41:13 +0530

    Update cpu.h

--- riscv_simulator/include/cpu.h
+++ riscv_simulator/include/cpu.h
@@ -4,17 +4,18 @@
 #include <vector>
 #include <unordered_map>
 #include <string>
+using namespace std; 
 
 const int NUM_REGS = 32;
 
 struct CPU {
     int core_id;
-    int registers[NUM_REGS] = {0};  
-    std::unordered_map<std::string, int> reg_map;
+    int registers[NUM_REGS] = {0};  // registers value set to zero
+    unordered_map<string, int> reg_map; // what is this for?
     int pc;  // Program Counter (PC)
 
     CPU(int id);
-    void executeInstruction(const std::string& opcode, const std::vector<std::string>& operands);
+    void executeInstruction(const std::string& opcode, const std::vector<std::string>& operands, const std::unordered_map<std::string, int>& label_map);
     void printRegisters();
 };
 
