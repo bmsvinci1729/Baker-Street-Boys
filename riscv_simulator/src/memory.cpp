2f435c400a7ad50ed2af9d9cba38c4eca4ad02a2
Sudhanva Bharadwaj BM <138512597+bmsvinci1729@users.noreply.github.com>
2025-02-20 00:43:51 +0530

    Update memory.cpp

--- riscv_simulator/src/memory.cpp
+++ riscv_simulator/src/memory.cpp
@@ -5,29 +5,33 @@ using namespace std;
 
 class Memory {
 public:
+    vector<int> memory;  // Simulated memory (4KB total)
+    
     Memory(int size);
+    
     int load(int address);
     void store(int address, int value);
     void printMemory();
-
-private:
-    vector<int> memory;
 };
 
 Memory::Memory(int size) : memory(size, 0) {}
 
 int Memory::load(int address) {
-    return memory[address];
+    return memory[address]; // so loads the value present at "address" in the memory vector
 }
 
 void Memory::store(int address, int value) {
-    memory[address] = value;
+    memory[address] = value; // value stored at address
 }
+
+
+// need to do the beautification part
 
 void Memory::printMemory() {
     std::cout << "Memory Dump:\n";
-    for (size_t i = 0; i < memory.size(); i++) {
+    for (int i = 0; i < memory.size(); i++) {
         if (memory[i] != 0)
             std::cout << "Memory[" << i << "]: " << memory[i] << "\n";
     }
 }
+ 
